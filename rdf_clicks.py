6e05e25439784daf33ae5f0dd8222cfd867e3d66
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-02-23 12:44:01 +0000

    Refactor of hashing function and insertion of datatype

--- rdf_clicks.py
+++ rdf_clicks.py
@@ -3,11 +3,11 @@
   and to create an ontology
 '''
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
-from rdflib.namespace import RDF, FOAF
+from rdflib.namespace import RDF, FOAF, XSD
 
 import hashlib
 
-FILE = "2017_01_en_clickstream.tsv"
+FILE = "/home/iain/wikipedia/2017_01_en_clickstream.tsv"
 SEARCHTERM = "Martin_Luther"
 
 g = Graph()
@@ -15,23 +15,29 @@ g = Graph()
 CLICK = Namespace("http://www.example.org/ns/click#")
 g.bind("click", CLICK)
 
+def _set_hash(hash_string):
+    '''
+       Create hash for URI
+    '''
+    return hashlib.sha224(hash_string).hexdigest()[15:]
+
 with open(FILE, 'rb') as f:
     data = f.readlines()
     for datum in data:
        line = datum.split("\t")
        if line[0] == SEARCHTERM:
-           m = hashlib.sha224(line[1]+"prev").hexdigest()[15:]
+           m = URIRef(_set_hash(line[1]+"prev"))
            g.add( (URIRef(line[1]), RDF.type, CLICK.link) )
-           g.add( (URIRef(m), RDF.type, URIRef(line[1])) )
-           g.add( (URIRef(m), CLICK.prev, URIRef(line[0])) )
-           g.add( (URIRef(m), CLICK.type, Literal(line[2])))
-           g.add( (URIRef(m), CLICK.weight, Literal(line[3][:-2])))
+           g.add( (m, RDF.type, URIRef(line[1])) )
+           g.add( (m, CLICK.prev, URIRef(line[0])) )
+           g.add( (m, CLICK.type, Literal(line[2])))
+           g.add( (m, CLICK.weight, Literal(line[3][:-2], datatype=XSD.integer)))
        elif line[1] == SEARCHTERM:
            g.add( (URIRef(line[0]), RDF.type, CLICK.link) )
-           m = hashlib.sha224(line[0]+"curr").hexdigest()[15:]
-           g.add( (URIRef(m), RDF.type, URIRef(line[0])) )
-           g.add( (URIRef(m), CLICK.curr, URIRef(line[1])) )
-           g.add( (URIRef(m), CLICK.type, Literal(line[2])))
-           g.add( (URIRef(m), CLICK.weight, Literal(line[3][:-2])))
+           m = URIRef(_set_hash(line[0]+"curr"))
+           g.add( (m, RDF.type, URIRef(line[0])) )
+           g.add( (m, CLICK.curr, URIRef(line[1])) )
+           g.add( (m, CLICK.type, Literal(line[2])))
+           g.add( (m, CLICK.weight, Literal(line[3][:-2], datatype=XSD.integer)))
 
 g.serialize(destination='luther.ttl', format='xml')
